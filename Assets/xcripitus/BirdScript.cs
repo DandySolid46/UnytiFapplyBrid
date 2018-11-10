fb13d757491739f4d0d89c016ff1d0deac894c42
DandySolid46 <lucasliduino@gmail.com>
2018-11-10 12:38:42 -0200

    rotacao

--- Assets/xcripitus/BirdScript.cs
+++ Assets/xcripitus/BirdScript.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
-
+using static UnityEngine.Mathf;
 public class BirdScript : MonoBehaviour {
 	Vector3 startPos;
 	public float speed = 200f;
 	Rigidbody2D rb2d;
+	public float rotationVelocity;
+	float rotation;
+	public float rotationFallTime;
+	
 
 	void Awake() {
 		rb2d = GetComponent<Rigidbody2D>();
@@ -14,16 +18,26 @@ public class BirdScript : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+		if ((rotation > -45)&Game.isPlaying){
+			rotation -= Pow(1.3f, rotationVelocity) * Time.deltaTime;
+			rotationVelocity += 1 ;
+		}
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			Game.isPlaying = rb2d.simulated = true;
 			rb2d.velocity = Vector2.up * speed ;
+			rotationVelocity = 0;
+			rotation = 45f;
 		} else if (!Game.isPlaying) {
 			rb2d.simulated = false;
 			rb2d.velocity = Vector2.zero;
 			var pos = transform.position;
 			pos.y = Mathf.Sin(Time.time * 4f) * 0.15f;
 			transform.position = pos;
+			rotation = 0;
 		}
+		var rot = transform.rotation;
+		rot.eulerAngles = new Vector3(0f, 0f, rotation);
+		transform.rotation = rot;
 	}
 
 	void OnCollisionEnter2D(Collision2D c) {
